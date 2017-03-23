d2662c1818e034c98b6ba929a0baee38056625db
Sébastien Muller <sebastien.muller@globalis-ms.com>
2017-03-23 14:58:00 +0100

    Slides d'alex

--- slides/alex.php
+++ slides/alex.php
@@ -1,34 +1,104 @@
 <section data-background-image='./lib/images/background_alex.png'>
     <section>
-        <h2>ALEX</h2>
+        <h6>Offre Agence</h6>
+        <h2>Notre offre</h2>
+
+        <ul>
+            <li>Intégration HTML / CSS</li>
+            <li>SEO</li>
+            <li>Accompagnement sur solutions tierces</li>
+            <li>Conseil</li>
+        </ul>
+
         <aside class="notes">
             Comments
         </aside>
     </section>
 
     <section>
-        <h2>ALEX</h2>
+        <h6>Offre Agence</h6>
+        <h2>Les 3 piliers du SEO</h2>
+
+        <ul>
+            <li>Le netlinking / backlink</li>
+            <li class="fragment">Le contenu</li>
+            <li class="fragment">La conception technique &amp; performance</li>
+        </ul>
+
         <aside class="notes">
             Comments
         </aside>
     </section>
 
     <section>
-        <h2>ALEX</h2>
+        <h6>Offre Agence</h6>
+        <h2>La stratégie</h2>
+
+        <div>
+            <span style="float: left">Montre nous ton site...</span>
+            <span style="float: right">...nous allons t'aider</span>
+        </div>
+        <br />
+        <div class="fragment">
+            <img src="./lib/images/alex/money.gif" alt="" style=" width: 600px; margin-bottom: 0.5em;" />
+            <p style="text-align: center; font-size: 1.4em;">Pour te faire consommer de la TMA!</p>
+        <div>
+
         <aside class="notes">
             Comments
         </aside>
     </section>
 
     <section>
-        <h2>ALEX</h2>
+        <h6>Offre Agence</h6>
+        <h2>Des outils en 2016</h2>
+
+        <div>
+            <img src="./lib/images/alex/botify.png" alt="Botify" style="border: none; width: 359px;" />
+            <img src="./lib/images/alex/botify-screen.png" alt="Botify" style="float: right; margin-left: 1em; border: none" />
+
+            <div style="float: left; width: 450px;">Crawler et outil d'analyses de log</div>
+        </div>
+
         <aside class="notes">
             Comments
         </aside>
     </section>
 
     <section>
-        <h2>ALEX</h2>
+        <h6>Offre Agence</h6>
+        <h2>Des outils en 2016</h2>
+
+        <div>
+            <img src="./lib/images/alex/uptrends.png" alt="Uptrends" style="border: none; width: 359px;" />
+            <img src="./lib/images/alex/uptrends-screen.png" alt="Uptrends" style="float: right; margin-left: 1em; border: none" />
+
+            <div style="float: left; width: 450px;">Outil de monitoring et temps de réponse / perf</div>
+        </div>
+
+        <aside class="notes">
+            Comments
+        </aside>
+    </section>
+
+    <section>
+        <h6>Offre Agence</h6>
+        <h2>En 2017</h2>
+
+        <div>
+            <img src="./lib/images/alex/2017.jpg" alt="" style="border: none; float: right; width: 40%;" />
+
+            <p style="float: left; width: 50%; white-space: nowrap;">
+                Sur un panel de 500 professionnels
+            </p>
+            <ul>
+                <li>6 / 10 > L'expérience utilisateur</li>
+                <li>5 / 10 > La web perf</li>
+                <li>3 / 10 > Le HTTPS</li>
+                <li>3 / 10 > Page AMP</li>
+            </ul>
+        </div>
+
         <aside class="notes">
             Comments
         </aside>
