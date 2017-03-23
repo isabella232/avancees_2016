299dc2b3d3bc39004aa00fe6bc99adfae269f30e
Sébastien Muller <sebastien.muller@globalis-ms.com>
2017-03-23 02:03:55 +0100

    Slides Esteban (validez le background, serieusement)

--- slides/esteban.php
+++ slides/esteban.php
@@ -1,36 +1,60 @@
-<section data-background-image='./lib/images/background_esteban.jpg'>
+<section data-background-image='./lib/images/background-esteban.png'>
     <section>
-        <h2>ESTEBAN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+        <h6>VM OVH Cloud</h6>
+        <h2>¿Qué pasa, amigo?</h2>
+        <ul>
+            <li>Les recettes sont souvent trop différentes de la prod</li>
+            <li>Complexe de répondre à certains besoins clients (ftp, ...)</li>
+            <li>Pouvoir gérer des outils spécifiques facilement</li>
+            <li>Eviter qu'un dev chez nous fasse tomber nos recettes.</li>
+        </ul>
     </section>
 
     <section>
-        <h2>ESTEBAN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+        <h6>VM OVH Cloud</h6>
+        <h2>Lo que necesita?</h2>
+        <ul>
+            <li>Des VM quand on veut, facilement, rapidement</li>
+            <li>Le plus iso prod possible : php, mysql, tools, config, etc</li>
+            <li>Rendre la chose automatisable à terme</li>
+        </ul>
     </section>
 
     <section>
-        <h2>ESTEBAN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+        <h6>VM OVH Cloud</h6>
+        <h2>Una solución, señor?</h2>
+        <ul>
+            <li>Oui, regarde le titre du slide.</li>
+            <li>Différents snapshots de serveurs préconfigurés</li>
+            <li>Des VM up & configurée en moins de 5 minutes</li>
+            <li>Une API REST pour automatiser les tasks : up, down, snapshot, reset, ...</li>
+        </ul>
     </section>
 
     <section>
-        <h2>ESTEBAN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+        <h6>VM OVH Cloud</h6>
+        <h2>¿Problemas, señor?</h2>
+        <ul>
+            <li>Une fois une VM configurée avec un snapshot, remettre à jour le snapshot de base est compliqué.</li>
+            <li>Une maintenance longue si on veut appliquer quelque chose partout</li>
+        </ul>
+
+        <div class="fragment">
+            <h4>¡Pero!</h4>
+            <ul>
+                <li>Néanmoins utilisés depuis plusieurs mois sur des projets&nbsp;!</li>
+                <li class="fragment">... Ok, surtout de la TMA.</li>
+            </ul>
+        </div>
     </section>
 
     <section>
-        <h2>ESTEBAN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+        <h6>VM OVH Cloud</h6>
+        <h2>Y en 2017?</h2>
+        <ul>
+            <li>Séparer les data des serveurs même : faciliter l'upgrade des serveurs</li>
+            <li>Automatiser la gestion : créer le subdomain, la VM, backup via snapshot, timer d'inactivité,&nbsp;...</li>
+            <li>Intégrer à Robo ?</li>
+        </ul>
     </section>
 </section>
