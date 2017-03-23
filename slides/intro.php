72d79c9eaf000e1c83f8b49a8d4f38b1201cd14f
Romain GUGERT <romain.gugert@globalis-ms.com>
2017-03-23 18:19:58 +0100

    Start techno slide

--- slides/intro.php
+++ slides/intro.php
@@ -21,4 +21,116 @@
             <li>...</li>
         </ul>
     </section>
+
+    <section>
+        <h2>Technos / Outils utilisées en 2016</h2>
+<img class="fragment" data-fragment-index="6" src="./lib/images/logo/apache.png" atl="apache" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="8" src="./lib/images/logo/bootstrap.png" atl="bootstrap" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="8" src="./lib/images/logo/codeigniter.png" atl="codeigniter" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/git.png" atl="git" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="7" src="./lib/images/logo/nodejs.png" atl="nodejs" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/gitlab.png" atl="gitlab" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="9" src="./lib/images/logo/gulp.png" atl="gulp" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="8" src="./lib/images/logo/jquery.png" atl="jquery" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="4" src="./lib/images/logo/mysql.jpg" atl="mysql" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="10" src="./lib/images/logo/phing.gif" atl="phing" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1" src="./lib/images/logo/php.png" atl="php" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/redmine.png" atl="redmine" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/rsync.png" atl="rsync" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="10" src="./lib/images/logo/sass.png" atl="sass" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="8" src="./lib/images/logo/wordpress.png" atl="wordpress" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="8" src="./lib/images/logo/zend.png" atl="zend" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/apidoc.png" atl="apidoc" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="9" src="./lib/images/logo/bower.svg" atl="bower" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="9" src="./lib/images/logo/composer.png" atl="composer" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/github.png" atl="github" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="11" src="./lib/images/logo/grunt.png" atl="grunt" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="2" src="./lib/images/logo/html_css_js.png" atl="html_css_js" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/make.jpeg" atl="make." style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="7" src="./lib/images/logo/nginx.png" atl="nginx" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="9" src="./lib/images/logo/npm.png" atl="npm" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="9" src="./lib/images/logo/phinx.png" atl="phinx" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/postman.png" atl="postman" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/robo.png" atl="robo" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="1000" src="./lib/images/logo/electron.png" atl="electron" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="10" src="./lib/images/logo/sage.png" atl="sage" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="8" src="./lib/images/logo/symfony.png" atl="symfony" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="9" src="./lib/images/logo/wp-cli.png" atl="wp-cli" style="border:none;width:10%;box-shadow: none;"/>
+<img class="fragment" data-fragment-index="5" src="./lib/images/logo/linux.png" atl="linux" style="border:none;width:10%;box-shadow: none;"/>
+<aside class="notes">
+    <ul>
+        <li>Classique - LAMP
+            <ul>
+                <li>PHP</li>
+                <li>HTML 5 - CSS 3 - Javascript</li>
+                <li>MySQL</li>
+                <li>Linux</li>
+                <li>Apache</li>
+            </ul>
+        </li>
+        <li>Alternatif
+            <ul>
+                <li>Node JS - nginx</li>
+            </ul>
+        </li>
+        <li> Librairies / Framework
+            <ul>
+                <li>bootstrap</li>
+                <li>jquery</li>
+                <li>codeigniter</li>
+                <li>wordpress</li>
+                <li>zend</li>
+                <li>symfony</li>
+                <li>carbone (@todo)</li>
+            </ul>
+        </li>
+        <li> Outil d'aide au dev
+            <ul>
+                <li>sage</li>
+                <li>
+            </ul>
+        </li>
+
+        <li> Outils dep
+            <ul>
+                <li>Composer</li>
+                <li>NPM</li>
+                <li>Gulp</li>
+                <li>Bower</li>
+                <li>grunt</li>
+            </ul>
+        </li>
+        <li>Gulp</li>
+        <li>Grunt</li>
+        <li>Bower</li>
+        <li>NPM</li>
+        <li>Composer</li>
+        <li>Sass</li>
+        <li>JQuery</li>
+        <li>Bootstrap 3</li>
+        <li>Bootstrap 4</li>
+        <li>Robo</li>
+        <li>Phinx</li>
+        <li>Phing</li>
+        <li>rsync</li>
+        <li>Worpress</li>
+        <li>CodeIgniter</li>
+        <li>Symfony</li>
+        <li>Zend Framework</li>
+        <li>Make</li>
+        <li>Git</li>
+        <li>Github</li>
+        <li>Redmine</li>
+        <li>GitLab</li>
+        <li>apidoc</li>
+        <li>Postman</li>
+        <li>wp-cli</li>
+        <li>sage-8</li>
+
+        <li>Carbon</li>
+        <li>VM OVH ???</li>
+    </ul>
+</aside>
+
+    </section>
 </section>
