ec822d3fcb26c17d9f41086b78e457911c17985f
Julien OGER <julien.oger@globali-ms.com>
2017-03-29 00:03:02 +0200

    Slides Julien

--- slides/julien.php
+++ slides/julien.php
@@ -1,36 +1,166 @@
-<section data-background-image='./lib/images/background_julien.jpg'>
+<section data-background-image='./lib/images/background_julien.jpg' style="opacity:1">
     <section>
-        <h2>JULIEN</h2>
+        <h2>La technique c'est bien...</h2>
+            <img src="./lib/images/god.gif" alt="" style="width: 800px; border:0" />
+            <div class="fragment" data-autoslide="4500">
+                <p style="text-align: center; font-size: 1.4em;" >...mais sans moi, le plateau n'est rien</p>
+                <p style="text-align: center; font-size: 0.8em;" >Instant mégalo</p>
+            </div>
+            <div class="fragment">
+
+            </div>
+
         <aside class="notes">
-            Comments
+            <ul>
+                <li>Nous avons aborder les avancées technos</li>
+                <li>A présent support orga et de méthode mis en place</li>
+            </ul>
         </aside>
     </section>
 
     <section>
-        <h2>JULIEN</h2>
+        <h6>Péréniser</h6>
+        <h2>L'organisation</h2>
+        <p style="text-align: center; font-size: 0.8em;" >2 pôles</p>
+        <div class="fragment">
+            <img src="./lib/images/orga1.jpg" alt="" style="border:0"/>
+        </div>
         <aside class="notes">
-            Comments
+            <ul>
+                <li>2015 - Réorganisation du plateau en pôle + équipes</li>
+                <li>2016 - Intégration d'une équipe front (a charger + problématique de staffing)</li>
+                <li>2017 - Faire gonfler les pôles avec 1 à 2 membres en plus</li>
+            </ul>
         </aside>
     </section>
 
-    <section>
-        <h2>JULIEN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+    <section data-transition="none">
+        <h6>Péréniser</h6>
+        <h2>L'organisation</h2>
+        <p style="text-align: center; font-size: 0.8em;" >5 équipes de développement</p>
+        <div>
+            <img src="./lib/images/orga2.jpg" alt="" style="border:0"/>
+        </div>
+    </section>
+
+    <section data-transition="none">
+        <h6>Péréniser</h6>
+        <h2>L'organisation</h2>
+        <p style="text-align: center; font-size: 0.8em;" >Equipes généralistes mais spécialisées</p>
+        <div>
+            <img src="./lib/images/orga3.jpg" alt="" style="border:0"/>
+        </div>
+    </section>
+
+    <section data-transition="none">
+        <h6>Péréniser</h6>
+        <h2>L'organisation</h2>
+        <p style="text-align: center; font-size: 0.8em;" >Perméabilité des membres</p>
+        <div>
+            <img src="./lib/images/orga4.jpg" alt="" style="border:0"/>
+        </div>
+    </section>
+
+    <section data-transition="none">
+        <h6>Péréniser</h6>
+        <h2>L'organisation</h2>
+        <p style="text-align: center; font-size: 0.8em;" >Perméabilité des types de projet</p>
+        <div>
+            <img src="./lib/images/orga5.jpg" alt="" style="border:0"/>
+        </div>
+    </section>
+
+    <section data-transition="none">
+        <h6>Péréniser</h6>
+        <h2>L'organisation</h2>
+        <p style="text-align: center; font-size: 0.8em;" >Objectif 2017 : faire gonfler les équipes</p>
+        <div>
+            <img src="./lib/images/orga6.jpg" alt="" style="border:0"/>
+        </div>
     </section>
 
     <section>
-        <h2>JULIEN</h2>
+        <h6>Amélioration continue</h6>
+        <h2>Méthode agile</h2>
+        <ul>
+            <li>A l'origine : Crystal Clear</li>
+            <li>Pour arriver à <span style="font-size: 0.5em">(presque)</span>Scrum</li>
+        </ul>
+        <div>
+            <img src="./lib/images/scrum.png" alt="" style=" width: 800px; border:0"/>
+        </div>
         <aside class="notes">
-            Comments
+          <ul>
+              <li>A l'origine : Crystal Clear / Méthode agile de base communication, itération dédiée à de petites équipes (mois de 6 personnes)</li>
+              <li>2014 -> 2015 Intégrer des éléments d'extreme programming et de Scrum : utilisation de redmine... + pilotage par les délais</li>
+              <li>2016 -> Scrum carnet de produit + gestion des suivis + organisation en SPrint</li>
+              <li>2017 -> Kanban apport de souplesse dans la gestion des Sprint</li>
+          </ul>
         </aside>
     </section>
 
-    <section>
-        <h2>JULIEN</h2>
-        <aside class="notes">
-            Comments
-        </aside>
+    <section data-transition="none">
+      <h6>Amélioration continue</h6>
+      <h2>Méthode agile</h2>
+        <p style="text-align: center; font-size: 0.8em;" >Des carnets de produit...</p>
+        <div>
+            <img src="./lib/images/suivi1.png" alt="" style="border:0"/>
+        </div>
     </section>
+
+    <section data-transition="none">
+      <h6>Amélioration continue</h6>
+      <h2>Méthode agile</h2>
+        <p style="text-align: center; font-size: 0.8em;" >...pour améliorer le suivi</p>
+        <div>
+            <img src="./lib/images/suivi2.png" alt="" style="border:0"/>
+        </div>
+    </section>
+
+    <section>
+      <h6>Rationnaliser</h6>
+      <h2>La veille technologique</h2>
+      <p>Objectifs</p>
+      <ul>
+          <li>Amélioration des process de production</li>
+          <li>Monter en compétence</li>
+          <li>Rester à jour</li>
+      </ul>
+      <aside class="notes">
+        <ul>
+            <li>Vision amélioration des process et des technos utilisées en interne : illustration par tout ce que viennent de dire les différents intervenants avant et par ce que va présenter rRomain juste après</li>
+            <li>Beaucoup plus de production utile pour 2016 : contribution d'Armel, de Nicolas et de Victor notamment</li>
+            <li>Des projets internes maintenus et du temps réservé à cela : puppet, front, MUCD</li>
+            <li>amélioration de la communication : création des Points Lead Bi-Mensuels</li>
+            <li>Résultat sur les projets ++++++ car contribu à moins d'échec</li>
+            <li>2017 : Continuer la dynamique et FORCE DE PROPOSITION + PARTICIPATION DE TOUS AUX VEILLES</li>
+        </ul>
+      </aside>
+    </section>
+
+    <section data-transition="none">
+      <h6>Rationnaliser</h6>
+      <h2>La veille technologique</h2>
+      <p>Mise en oeuvre 2016</p>
+      <ul>
+          <li>Projets interne : Puppet, MUCD, Globox</li>
+          <li>Publications (Armel, Nico, Victor notamment)</li>
+          <li>Formations certifiantes</li>
+          <li>Point Lead bi-mensuel</li>
+      </ul>
+    </section>
+
+    <section data-transition="none">
+      <h6>Rationnaliser</h6>
+      <h2>La veille technologique</h2>
+      <p>Objectif 2017</p>
+      <ul>
+          <li>Garder la même dynamique</li>
+          <li>Participez et proposez !!!</li>
+      </ul>
+      <div>
+          <img src="./lib/images/team.gif" alt="" style="border:0"/>
+      </div>
+    </section>
+
 </section>
